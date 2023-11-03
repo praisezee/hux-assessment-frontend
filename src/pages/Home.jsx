35f4583ec22a174f871f6a9cad8ca3eee999d5ba
Praise Folorunso <folorunsopraise580@gmail.com>
2023-11-03 07:59:10 +0100

    changes html title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import LoginModal from "../components/LoginModal";
 import RegisterModal from "../components/RegisterModal";
 
@@ -5,6 +6,17 @@ const Home = () =>
 {
   return (
     <>
+      <div className="vh-75 d-flex flex-column justify-content-center align-items-center p-5">
+        <p className="h3 text-capitalize fw-bold">
+          contact Storage
+        </p>
+        <p className="text-muted h5">Looking for an online contact storage? Here is the right application for you. Register to explore full experience</p>
+        <p className="h6 fw-bold text-uppercase">System designed by <span className="h4 fw-bolder">
+          <Link to='https://folorunsopraise.vercel.app' target="_blank" className="link-dark">
+            Folorunso Praise
+          </Link>
+        </span></p>
+      </div>
       <LoginModal />
       <RegisterModal/>
     </>
